d88e5740bdb4380499ff38193df2f4d3813c1372
Shost_Xe04 <major01510@gmail.com>
2025-10-18 15:33:02 +0300

    Project reset: keep my current version only

---
+++ src/pages/Group/ui/ProductCard.jsx
@@ -0,0 +1,50 @@
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import AppContext from "../../../features/context/AppContext";
+import './ProductCard.css';
+
+export default function ProductCard({ product, isAssociation }) {
+    const { cart, request, updateCart } = useContext(AppContext);
+    const navigate = useNavigate();
+    const isInCart = cart.cartItems.some(ci => ci.productId == product.id);
+
+    const addToCartClick = e => {
+        e.preventDefault();
+        request("/api/cart/" + product.id, { method: 'POST' })
+            .then(() => updateCart())
+            .catch(() => {});
+    };
+
+    const goToCartClick = e => {
+        e.preventDefault();
+        navigate("/cart");
+    };
+
+    return (
+        <div className="col">
+            <Link to={"/product/" + (product.slug || product.id)} className="nav-link h-100">
+                <div className={"card h-100" + (isAssociation ? " association" : "")}>
+                    <img src={product.imageUrl} className="card-img-top" alt={product.name} />
+                    <div className="card-body">
+                        <h5 className="card-title">{product.name}</h5>
+                        <p className="card-text">{product.description}</p>
+                    </div>
+                    <div className="card-footer bg-transparent d-flex justify-content-between align-items-center">
+                        <span>₴ {product.price.toFixed(2)}</span>
+                        <div data-in-cart={isInCart ? "1" : "0"}>
+                            {isInCart ? (
+                                <button onClick={goToCartClick} className="in-cart btn btn-success">
+                                    <i className="bi bi-cart-check"></i>
+                                </button>
+                            ) : (
+                                <button onClick={addToCartClick} data-product-id={product.id} className="not-in-cart btn btn-outline-success">
+                                    <i className="bi bi-cart"></i>
+                                </button>
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </Link>
+        </div>
+    );
+}
